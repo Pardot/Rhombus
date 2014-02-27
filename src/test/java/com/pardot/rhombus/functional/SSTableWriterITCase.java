6e97cfddb4b78bea7c75d84580dcd5e4fd71aa1d
Dan van Kley <dvankley@gmail.com>
2014-02-27 14:34:30 -0500

    PDT-3884 SSTableWriter gets pissy if you try to build two SSTableWriters with the same table/columnfamily name, so just use different table names for each test

--- src/test/java/com/pardot/rhombus/functional/SSTableWriterITCase.java
+++ src/test/java/com/pardot/rhombus/functional/SSTableWriterITCase.java
@@ -30,6 +30,8 @@ public class SSTableWriterITCase extends RhombusFunctionalTest {
     public void testInsertingAllNonNullValuesInSchema_simple() throws Exception {
         logger.debug("Starting testInsertingAllNonNullValuesInSchema");
         System.setProperty("cassandra.config", "cassandra-config/cassandra.yaml");
+        String defaultTableName = "simple";
+        String testUniqueTableName = "simpleA";
 
         //Build the connection manager
         ConnectionManager cm = getConnectionManager();
@@ -39,6 +41,13 @@ public class SSTableWriterITCase extends RhombusFunctionalTest {
         assertNotNull(keyspaceDefinition);
         String keyspaceName = keyspaceDefinition.getName();
         ShardingStrategyMonthly shardStrategy = new ShardingStrategyMonthly();
+
+        // SSTableWriter craps out if we try to close a writer on a table and then create a new one on the same table, so each test should write to different tables
+        Map<String, CDefinition> tableDefs = keyspaceDefinition.getDefinitions();
+        CDefinition def = tableDefs.get(defaultTableName);
+        def.setName(testUniqueTableName);
+        tableDefs.remove(defaultTableName);
+        tableDefs.put(testUniqueTableName, def);
 
         // Make sure the SSTableOutput directory exists and is clear
         File keyspaceDir = new File(keyspaceName);
@@ -55,7 +64,7 @@ public class SSTableWriterITCase extends RhombusFunctionalTest {
         om.setLogCql(true);
 
         // This is the only static table definition this test keyspace has
-        List<String> staticTableNames = Arrays.asList("simple");
+        List<String> staticTableNames = Arrays.asList(testUniqueTableName);
         Map<String, CIndex> indexes = new HashMap<String, CIndex>();
         Map<String, String> indexTableToStaticTableName = new HashMap<String, String>();
         // Pull the definition and associated indexes for each static table
@@ -89,6 +98,113 @@ public class SSTableWriterITCase extends RhombusFunctionalTest {
             insert.put(indexTableName, values);
         }
         // Actually insert the data into the SSTableWriters
+        om.insertIntoSSTable(insert, indexes, indexTableToStaticTableName);
+        assertTrue(om.completeSSTableWrites());
+
+        // Make a list of all table names so we can load them into Cassandra
+        List<String> allTableNames = new ArrayList<String>(indexes.keySet());
+        allTableNames.addAll(staticTableNames);
+        for (String tableName : allTableNames) {
+            String SSTablePath = keyspaceName + "/" + tableName;
+
+            // Load the SSTables into Cassandra
+            ProcessBuilder builder = new ProcessBuilder("sstableloader", "-d", "localhost", SSTablePath);
+            builder.redirectErrorStream(true);
+            Process p = builder.start();
+            BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            // TODO: sleep is the devil
+            while (!r.readLine().contains("100%")) {
+                Thread.sleep(100);
+            }
+        }
+
+        String staticTableName = staticTableNames.get(0);
+        for (Map<String, Object> expected : values) {
+            Map<String, Object> actual = om.getByKey(staticTableName, expected.get("id"));
+            actual.put("shardid", shardStrategy.getShardKey(Long.parseLong(actual.get("created_at").toString(), 10)));
+            assertEquals(expected, actual);
+        }
+
+        // Clean up the SSTable directories after ourselves
+        FileUtils.deleteRecursive(new File(keyspaceName));
+    }
+
+    @Test(expected=NullPointerException.class)
+    public void testInsertingSomeNullValuesInSchema_simple() throws Exception {
+        logger.debug("Starting testInsertingAllNonNullValuesInSchema");
+        System.setProperty("cassandra.config", "cassandra-config/cassandra.yaml");
+        String defaultTableName = "simple";
+        String testUniqueTableName = "simpleB";
+
+        //Build the connection manager
+        ConnectionManager cm = getConnectionManager();
+
+        //Build our keyspace definition object
+        CKeyspaceDefinition keyspaceDefinition = JsonUtil.objectFromJsonResource(CKeyspaceDefinition.class, this.getClass().getClassLoader(), "TableWriterSimpleKeyspace.js");
+        assertNotNull(keyspaceDefinition);
+        String keyspaceName = keyspaceDefinition.getName();
+
+        // SSTableWriter craps out if we try to close a writer on a table and then create a new one on the same table, so each test should write to different tables
+        Map<String, CDefinition> tableDefs = keyspaceDefinition.getDefinitions();
+        CDefinition def = tableDefs.get(defaultTableName);
+        def.setName(testUniqueTableName);
+        tableDefs.remove(defaultTableName);
+        tableDefs.put(testUniqueTableName, def);
+
+        ShardingStrategyMonthly shardStrategy = new ShardingStrategyMonthly();
+
+        // Make sure the SSTableOutput directory exists and is clear
+        File keyspaceDir = new File(keyspaceName);
+        if (keyspaceDir.exists()) {
+            FileUtils.deleteRecursive(new File(keyspaceName));
+        }
+        assertTrue(new File(keyspaceName).mkdir());
+
+        //Rebuild the keyspace and get the object mapper
+        cm.buildKeyspace(keyspaceDefinition, true);
+        logger.debug("Built keyspace: {}", keyspaceDefinition.getName());
+        cm.setDefaultKeyspace(keyspaceDefinition);
+        ObjectMapper om = cm.getObjectMapper();
+        om.setLogCql(true);
+
+        // This is the only static table definition this test keyspace has
+        List<String> staticTableNames = Arrays.asList(testUniqueTableName);
+        Map<String, CIndex> indexes = new HashMap<String, CIndex>();
+        Map<String, String> indexTableToStaticTableName = new HashMap<String, String>();
+        // Pull the definition and associated indexes for each static table
+        for (String staticTableName : staticTableNames) {
+            CDefinition definition = keyspaceDefinition.getDefinitions().get(staticTableName);
+            List<CIndex> indexDefinitions = definition.getIndexesAsList();
+            for (CIndex index : indexDefinitions) {
+                String indexTableName = CObjectCQLGenerator.makeTableName(definition, index);
+                indexes.put(indexTableName, index);
+                indexTableToStaticTableName.put(indexTableName, staticTableName);
+            }
+        }
+
+        //Insert our test data into the SSTable
+        // For this test, all this data goes into the one table we have defined
+        List<Map<String, Object>> values = JsonUtil.rhombusMapFromResource(this.getClass().getClassLoader(), "SSTableWriterSimpleTestData.js");
+        // Tack on time based UUIDs because we don't really care what the UUID values are
+        for (Map<String, Object> map : values) {
+            map.put("id", UUIDs.startOf(Long.parseLong(map.get("created_at").toString(), 10)));
+            // For this test, remove the actual values so we see what happens if we try to insert nulls
+            map.remove("value");
+        }
+        // Build the map to insert that we'll actually pass in
+        Map<String, List<Map<String, Object>>> insert = new HashMap<String, List<Map<String, Object>>>();
+        for (String staticTableName : staticTableNames) {
+            insert.put(staticTableName, values);
+        }
+        // Add in shardId for index tables
+        for (Map<String, Object> map : values) {
+            map.put("shardid", shardStrategy.getShardKey(Long.parseLong(map.get("created_at").toString(), 10)));
+        }
+        for (String indexTableName : indexes.keySet()) {
+            insert.put(indexTableName, values);
+        }
+        // Actually insert the data into the SSTableWriters
+        // NPE happens here, should be fixed in Cassandra 2.0.6 per https://issues.apache.org/jira/browse/CASSANDRA-6526
         om.insertIntoSSTable(insert, indexes, indexTableToStaticTableName);
         assertTrue(om.completeSSTableWrites());
 
