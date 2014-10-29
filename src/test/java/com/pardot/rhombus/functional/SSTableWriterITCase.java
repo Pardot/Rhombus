1e27899227d38254c17eea1b57528fb31c8503e1
Dan van Kley <dvankley@salesforce.com>
2014-10-29 18:34:33 -0400

    This test shouldn't be run live.

--- src/test/java/com/pardot/rhombus/functional/SSTableWriterITCase.java
+++ src/test/java/com/pardot/rhombus/functional/SSTableWriterITCase.java
@@ -13,6 +13,7 @@ import com.pardot.rhombus.cobject.shardingstrategy.ShardingStrategyMonthly;
 import com.pardot.rhombus.cobject.shardingstrategy.ShardingStrategyNone;
 import com.pardot.rhombus.util.JsonUtil;
 import org.apache.cassandra.io.util.FileUtils;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,6 +25,7 @@ import java.util.*;
 
 import static org.junit.Assert.*;
 
+@Ignore
 public class SSTableWriterITCase extends RhombusFunctionalTest {
 
     private static Logger logger = LoggerFactory.getLogger(ObjectMapperUpdateITCase.class);
